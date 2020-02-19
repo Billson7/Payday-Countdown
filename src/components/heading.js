7405fc41a5d70ab9429a21f2e777f4352be37749
Billson7 <alex.billson@sky.uk>
2020-02-19 13:15:43 +0000

    There for potential update

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,17 +1,17 @@
 const heading = () => {
   const months = [
-    { month: "January", payDate: "27/01/2020" },
-    { month: "February", payDate: "02/28/2020" },
-    { month: "March", payDate: "03/29/2020" },
-    { month: "April", payDate: "04/30/2020" },
-    { month: "May", payDate: "05/31/2020" },
-    { month: "June", payDate: "06/28/2020" },
-    { month: "July", payDate: "07/31/2020" },
-    { month: "August", payDate: "08/30/2020" },
-    { month: "September", payDate: "09/30/2020" },
-    { month: "October", payDate: "10/31/2020" },
-    { month: "November", payDate: "11/29/2020" },
-    { month: "Decemeber", payDate: "12/24/2020" }
+    { month: "January", payDate: "27/01/2020", time: "09:00:00" },
+    { month: "February", payDate: "02/28/2020" , time: "09:00:00"},
+    { month: "March", payDate: "03/29/2020", time: "09:00:00" },
+    { month: "April", payDate: "04/30/2020", time: "09:00:00" },
+    { month: "May", payDate: "05/31/2020", time: "09:00:00" },
+    { month: "June", payDate: "06/28/2020", time: "09:00:00" },
+    { month: "July", payDate: "07/31/2020", time: "09:00:00" },
+    { month: "August", payDate: "08/30/2020", time: "09:00:00" },
+    { month: "September", payDate: "09/30/2020", time: "09:00:00" },
+    { month: "October", payDate: "10/31/2020", time: "09:00:00" },
+    { month: "November", payDate: "11/29/2020", time: "09:00:00" },
+    { month: "Decemeber", payDate: "12/24/2020", time: "09:00:00" }
   ];
   const d = new Date();
   return months[d.getMonth()];
