b60f010d7238567bfedfd7c5a6a5348109e4fc3d
Billson7 <alex.billson@sky.uk>
2020-06-09 16:52:33 +0100

    Seperating components

--- src/App.js
+++ src/App.js
@@ -1,53 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import heading from "./components/heading";
-import HeadingGreeting from "./components/heading-greeting";
-
+import Heading from "./components/Heading";
+import HeadingGreeting from "./components/HeadingGreeting";
+import Timer from "./components/Timer";
 const App = () => {
-  const { month, payDate } = heading();
-  const calculateTimeLeft = () => {
-    const difference = +new Date(payDate) - +new Date();
-    let timeLeft = {};
-
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hrs: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        mins: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
-    }
-    return timeLeft;
-  };
-
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-  useEffect(() => {
-    setTimeout(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
-  });
-  const timerComponents = [];
-
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
-      return;
-    }
-
-    timerComponents.push(
-      <span id="time" key={interval}>
-        {timeLeft[interval]} {interval}{" "}
-      </span>
-    );
-  });
+  const { month } = Heading();
 
   return (
     <div className="page-section">
       <div className="countdown-page">
-        <h1 id="header">{HeadingGreeting()}, </h1>
-        <h1 id="header">this is a countdown to your {month} Payday!</h1>
-        <div className="timer">
-          {timerComponents.length ? timerComponents : <span>Payday!</span>}
-        </div>
+        <Timer />
+        <h1 id="header">
+          {HeadingGreeting()}, this is a countdown to your {month} Payday!
+        </h1>
       </div>
     </div>
   );
---
+++ src/components/Timer.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import Heading from "./Heading";
+
+const Timer = () => {
+  const { payDate } = Heading();
+  const calculateTimeLeft = () => {
+    const difference = +new Date(payDate) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        Days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        Hrs: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        Mins: Math.floor((difference / 1000 / 60) % 60),
+        Seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span id="time" key={interval}>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  return (
+    <div className="timer">
+      {timerComponents.length ? timerComponents : <span>Payday!</span>}
+    </div>
+  );
+};
+export default Timer;
