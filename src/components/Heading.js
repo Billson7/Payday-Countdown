221ce4d06289dcd4ca153fdb9c925e782da4def4
Alex Billson <57901471+Billson7@users.noreply.github.com>
2020-12-30 14:24:53 +0000

    Updated pay dates to 2021

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,17 +1,17 @@
 const Heading = () => {
-  const months = [
-    { month: "January", payDate: "01/31/2020", time: "09:00:00" },
-    { month: "February", payDate: "02/28/2020", time: "09:00:00" },
-    { month: "March", payDate: "03/31/2020", time: "09:00:00" },
-    { month: "April", payDate: "04/30/2020", time: "09:00:00" },
-    { month: "May", payDate: "05/29/2020", time: "09:00:00" },
-    { month: "June", payDate: "06/30/2020", time: "09:00:00" },
-    { month: "July", payDate: "07/31/2020", time: "09:00:00" },
-    { month: "August", payDate: "08/28/2020", time: "09:00:00" },
-    { month: "September", payDate: "09/30/2020", time: "09:00:00" },
-    { month: "October", payDate: "10/30/2020", time: "09:00:00" },
-    { month: "November", payDate: "11/30/2020", time: "09:00:00" },
-    { month: "Decemeber", payDate: "12/24/2020", time: "09:00:00" },
+ const months = [
+    { month: "January", payDate: "01/29/2021", time: "00:00:00" },
+    { month: "February", payDate: "02/26/2021", time: "00:00:00" },
+    { month: "March", payDate: "03/31/2021", time: "00:00:00" },
+    { month: "April", payDate: "04/30/2021", time: "00:00:00" },
+    { month: "May", payDate: "05/28/2021", time: "00:00:00" },
+    { month: "June", payDate: "06/30/2021", time: "00:00:00" },
+    { month: "July", payDate: "07/30/2021", time: "00:00:00" },
+    { month: "August", payDate: "08/31/2021", time: "00:00:00" },
+    { month: "September", payDate: "00/30/2021", time: "00:00:00" },
+    { month: "October", payDate: "10/29/2021", time: "00:00:00" },
+    { month: "November", payDate: "11/30/2021", time: "00:00:00" },
+    { month: "December", payDate: "12/24/2021", time: "00:00:00" }
   ];
   const d = new Date();
   return months[d.getMonth()];
