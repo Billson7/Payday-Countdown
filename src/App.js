9116debb3f4865b620f1ab7a7ee5bf67857672a0
Alex Billson <alex.billson@sky.uk>
2021-04-15 17:44:06 +0100

    Added Prettier for formatting

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ const App = () => {
   const { month, payDate } = Heading();
 
   return (
-    <body className='min-h-screen bg-white dark:bg-gray-900 '>
+    <body className="min-h-screen bg-white dark:bg-gray-900 ">
       <div className="container bg-white dark:bg-gray-900 pt-40 md:pt-0">
         <div className="countdown-page">
           <Timer />
-          <h1 className='text-black dark:text-white' id="header">
+          <h1 className="text-black dark:text-white" id="header">
             {HeadingGreeting()}, this is a countdown to your {month} Payday!
-        </h1>
+          </h1>
           <p className="mt-2 text-gray-300 dark:text-white">{payDate}</p>
         </div>
       </div>
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -18,30 +18,28 @@ const Timer = () => {
     return timeLeft;
   };
   const timerFormatter = () => {
-    let day = timeLeft.day
-    let hour = timeLeft.hour
-    let minute = timeLeft.minute
-    let seconds = timeLeft.seconds
+    let day = timeLeft.day;
+    let hour = timeLeft.hour;
+    let minute = timeLeft.minute;
+    let seconds = timeLeft.seconds;
     // Checks if seconds is equal ro 0 and replaces it with double zero
-    let secondsZeroCheck = seconds !== 0 ? seconds : "00"
+    let secondsZeroCheck = seconds !== 0 ? seconds : "00";
     // adds a starting zero if the number drops below 10 so it show 09, 08 etc
-    secondsZeroCheck = seconds < 10 ? "0" + seconds : seconds
+    secondsZeroCheck = seconds < 10 ? "0" + seconds : seconds;
 
-    let minuteZeroCheck = minute !== 0 ? minute : "00"
-    minuteZeroCheck = minute < 10 ? "0" + minute : minute
+    let minuteZeroCheck = minute !== 0 ? minute : "00";
+    minuteZeroCheck = minute < 10 ? "0" + minute : minute;
 
-    let hourZeroCheck = hour !== 0 ? hour : "00"
-    hourZeroCheck = hour < 10 ? "0" + hour : hour
+    let hourZeroCheck = hour !== 0 ? hour : "00";
+    hourZeroCheck = hour < 10 ? "0" + hour : hour;
 
-    let dayZeroCheck = day !== 0 ? day : "00"
-    dayZeroCheck = day < 10 ? "0" + day : day
+    let dayZeroCheck = day !== 0 ? day : "00";
+    dayZeroCheck = day < 10 ? "0" + day : day;
 
-    let seperator = ":"
-    const timer = dayZeroCheck += seperator += hourZeroCheck += seperator += minuteZeroCheck += seperator += secondsZeroCheck
-    console.log("TIMER", timer)
-    return timer
-
-  }
+    let seperator = ":";
+    const timer = (dayZeroCheck += seperator += hourZeroCheck += seperator += minuteZeroCheck += seperator += secondsZeroCheck);
+    return timer;
+  };
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
   useEffect(() => {
@@ -57,19 +55,21 @@ const Timer = () => {
     }
 
     timerComponents.push(
-      <span className='hidden' id="time" key={interval}>
-        {timeLeft[interval] < 10 ? "0" + timeLeft[interval] : timeLeft[interval]}
+      <span className="hidden" id="time" key={interval}>
+        {timeLeft[interval] < 10
+          ? "0" + timeLeft[interval]
+          : timeLeft[interval]}
       </span>
     );
   });
 
   return (
     <div className="timer" data-testid="timer">
-      <span className='bg-black dark:bg-white p-3 rounded-lg text-white dark:text-black font-extralight'>
+      <span className="bg-black dark:bg-white p-3 rounded-lg text-white dark:text-black font-extralight">
         {timerFormatter()}
-      </span >
-      { timerComponents.length ? timerComponents : <span>Payday!</span>}
-    </div >
+      </span>
+      {timerComponents.length ? timerComponents : <span>Payday!</span>}
+    </div>
   );
 };
 export default Timer;
