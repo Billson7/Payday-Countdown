dd2cd0d72755df42276b2702a784b694169282c2
Alex Billson <57901471+Billson7@users.noreply.github.com>
2021-09-13 17:54:16 +0100

    Fixing September date typo

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -8,7 +8,7 @@ const Heading = () => {
     { month: "June", payDate: "06/30/2021", time: "00:00:00" },
     { month: "July", payDate: "07/30/2021", time: "00:00:00" },
     { month: "August", payDate: "08/31/2021", time: "00:00:00" },
-    { month: "September", payDate: "00/30/2021", time: "00:00:00" },
+    { month: "September", payDate: "09/30/2021", time: "00:00:00" },
     { month: "October", payDate: "10/29/2021", time: "00:00:00" },
     { month: "November", payDate: "11/30/2021", time: "00:00:00" },
     { month: "December", payDate: "12/24/2021", time: "00:00:00" }
