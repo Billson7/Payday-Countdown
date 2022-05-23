5d8baf2b0f1161391ca3bbcf17801ce88f41e6c0
Alex Billson <57901471+Billson7@users.noreply.github.com>
2022-05-23 14:52:19 +0100

    reverting a bad commit

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,14 +1,53 @@
 import React, { useEffect, useState } from "react";
-import { TimerFormatter, CalculateTimeLeft } from '../utils/timer-utils'
+import { Heading } from "./Heading";
 
 export const Timer = () => {
-  const [timeLeft, setTimeLeft] = useState(CalculateTimeLeft(timeLeft));
+  const { payDate } = Heading();
+  const calculateTimeLeft = () => {
+    const difference = +new Date(payDate) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        day: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hour: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minute: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+    return timeLeft;
+  };
+  const timerFormatter = () => {
+    let day = timeLeft.day;
+    let hour = timeLeft.hour;
+    let minute = timeLeft.minute;
+    let seconds = timeLeft.seconds;
+    // Checks if seconds is equal ro 0 and replaces it with double zero
+    let secondsZeroCheck = seconds !== 0 ? seconds : "00";
+    // adds a starting zero if the number drops below 10 so it show 09, 08 etc
+    secondsZeroCheck = seconds < 10 ? "0" + seconds : seconds;
+
+    let minuteZeroCheck = minute !== 0 ? minute : "00";
+    minuteZeroCheck = minute < 10 ? "0" + minute : minute;
+
+    let hourZeroCheck = hour !== 0 ? hour : "00";
+    hourZeroCheck = hour < 10 ? "0" + hour : hour;
+
+    let dayZeroCheck = day !== 0 ? day : "00";
+    dayZeroCheck = day < 10 ? "0" + day : day;
+
+    let seperator = ":";
+    const timer = (dayZeroCheck += seperator += hourZeroCheck += seperator += minuteZeroCheck += seperator += secondsZeroCheck);
+
+    return timer.includes("undefined") ? "Payday!" : timer;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
   useEffect(() => {
     setTimeout(() => {
-      setTimeLeft(CalculateTimeLeft());
+      setTimeLeft(calculateTimeLeft());
     }, 1000);
   });
-
   const timerComponents = [];
 
   Object.keys(timeLeft).forEach((interval) => {
@@ -28,7 +67,7 @@ export const Timer = () => {
   return (
     <div className="timer" data-testid="timer">
       <span className="bg-black dark:bg-white p-3 rounded-lg text-white dark:text-black font-extralight">
-        {TimerFormatter()}
+        {timerFormatter()}
       </span>
     </div>
   );
