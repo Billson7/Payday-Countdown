739457f8ad0b1d0a94190a3f77e8c4b1452f8283
Alex Billson <alex.billson@sky.uk>
2023-01-04 16:08:06 +0000

    implicit return on get pay date checker

--- src/utils/get-pay-date-checker.js
+++ src/utils/get-pay-date-checker.js
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-export const getPayDateChecker = (payDate) => {
-  const todaysDate = moment().format("L");
-  return todaysDate === payDate ? "" : "a countdown to";
-};
+const todaysDate = moment().format("L");
+
+export const getPayDateChecker = (payDate) =>
+  todaysDate !== payDate ? "a countdown to" : "";
