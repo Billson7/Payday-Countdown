b616b66dcf2b7d2ecd80811f6c50c61e53caf129
Alex Billson <alex.billson@sky.uk>
2023-01-04 15:41:36 +0000

    Guess at 2023 dates

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,17 +1,17 @@
 export const Heading = () => {
  const months = [
     { month: "January", payDate: "01/27/2023", time: "00:00:00" },
-    { month: "February", payDate: "02/25/2023", time: "00:00:00" },
+    { month: "February", payDate: "02/24/2023", time: "00:00:00" },
     { month: "March", payDate: "03/31/2023", time: "00:00:00" },
-    { month: "April", payDate: "04/27/2023", time: "00:00:00" },
-    { month: "May", payDate: "05/27/2023", time: "00:00:00" },
-    { month: "June", payDate: "06/27/2023", time: "00:00:00" },
-    { month: "July", payDate: "07/27/2023", time: "00:00:00" },
-    { month: "August", payDate: "08/26/2023", time: "00:00:00" },
-    { month: "September", payDate: "09/27/2023", time: "00:00:00" },
+    { month: "April", payDate: "04/28/2023", time: "00:00:00" },
+    { month: "May", payDate: "05/26/2023", time: "00:00:00" },
+    { month: "June", payDate: "06/30/2023", time: "00:00:00" },
+    { month: "July", payDate: "07/28/2023", time: "00:00:00" },
+    { month: "August", payDate: "08/25/2023", time: "00:00:00" },
+    { month: "September", payDate: "09/29/2023", time: "00:00:00" },
     { month: "October", payDate: "10/27/2023", time: "00:00:00" },
-    { month: "November", payDate: "11/25/2023", time: "00:00:00" },
-    { month: "Christmas", payDate: "12/21/2023", time: "00:00:00" }
+    { month: "November", payDate: "11/30/2023", time: "00:00:00" },
+    { month: "Christmas", payDate: "12/22/2023", time: "00:00:00" }
   ];
   const d = new Date();
   return months[d.getMonth()];
