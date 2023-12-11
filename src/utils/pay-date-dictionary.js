29496f4ea85d3927bedb0a862e2a2e47419c9f0a
Alex Billson <alex.billson@sky.uk>
2023-12-11 09:04:20 +0000

    december date

--- src/utils/pay-date-dictionary.js
+++ src/utils/pay-date-dictionary.js
@@ -12,5 +12,5 @@ export const payDateDictionary = [
   { month: "September", payDate: `09/25/${currentYear}`, time: "00:00:00" },
   { month: "October", payDate: `10/27/${currentYear}`, time: "00:00:00" },
   { month: "November", payDate: `11/27/${currentYear}`, time: "00:00:00" },
-  { month: "Christmas", payDate: `12/22/${currentYear}`, time: "00:00:00" },
+  { month: "Christmas", payDate: `12/21/${currentYear}`, time: "00:00:00" },
 ];
