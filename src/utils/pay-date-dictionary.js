12cecb96b08771080a10f3ef5a23ad2b23a2bfc7
Alex Billson <alex.billson@sky.uk>
2024-04-12 09:42:49 +0100

    corrected january date

--- src/utils/pay-date-dictionary.js
+++ src/utils/pay-date-dictionary.js
@@ -4,7 +4,7 @@ export const payDateDictionary = [
   { month: "January", payDate: `01/26/${currentYear}`, time: "00:00:00" },
   { month: "February", payDate: `02/27/${currentYear}`, time: "00:00:00" },
   { month: "March", payDate: `03/27/${currentYear}`, time: "00:00:00" },
-  { month: "April", payDate: `04/27/${currentYear}`, time: "00:00:00" },
+  { month: "April", payDate: `04/24/${currentYear}`, time: "00:00:00" },
   { month: "May", payDate: `05/26/${currentYear}`, time: "00:00:00" },
   { month: "June", payDate: `06/27/${currentYear}`, time: "00:00:00" },
   { month: "July", payDate: `07/27/${currentYear}`, time: "00:00:00" },
