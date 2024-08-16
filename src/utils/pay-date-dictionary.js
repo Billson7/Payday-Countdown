8aa0f0070dd3455da647408e9afaa37578a5f00f
Alex Billson <alex.billson@sky.uk>
2024-08-16 11:47:43 +0100

    august pay day

--- src/utils/pay-date-dictionary.js
+++ src/utils/pay-date-dictionary.js
@@ -8,7 +8,7 @@ export const payDateDictionary = [
   { month: "May", payDate: `05/24/${currentYear}`, time: "00:00:00" },
   { month: "June", payDate: `06/27/${currentYear}`, time: "00:00:00" },
   { month: "July", payDate: `07/26/${currentYear}`, time: "00:00:00" },
-  { month: "August", payDate: `08/25/${currentYear}`, time: "00:00:00" },
+  { month: "August", payDate: `08/27/${currentYear}`, time: "00:00:00" },
   { month: "September", payDate: `09/25/${currentYear}`, time: "00:00:00" },
   { month: "October", payDate: `10/27/${currentYear}`, time: "00:00:00" },
   { month: "November", payDate: `11/27/${currentYear}`, time: "00:00:00" },
