dcb49704e0492e2967cb9b9ddbcf62e94708b050
Alex Billson <alex.billson@sky.uk>
2024-09-03 15:29:12 +0100

    september pay day

--- src/utils/pay-date-dictionary.js
+++ src/utils/pay-date-dictionary.js
@@ -9,7 +9,7 @@ export const payDateDictionary = [
   { month: "June", payDate: `06/27/${currentYear}`, time: "00:00:00" },
   { month: "July", payDate: `07/26/${currentYear}`, time: "00:00:00" },
   { month: "August", payDate: `08/27/${currentYear}`, time: "00:00:00" },
-  { month: "September", payDate: `09/25/${currentYear}`, time: "00:00:00" },
+  { month: "September", payDate: `09/27/${currentYear}`, time: "00:00:00" },
   { month: "October", payDate: `10/27/${currentYear}`, time: "00:00:00" },
   { month: "November", payDate: `11/27/${currentYear}`, time: "00:00:00" },
   { month: "Christmas", payDate: `12/21/${currentYear}`, time: "00:00:00" },
